8972d69097588c6bf98e3c04658bb581c86f9779
Steph0522 <shereirap@gmail.com>
2022-08-05 12:11:36 -0500

    Last changes in function

--- R/2022_08_05-READ-read_dbCAN.R
+++ R/2022_08_05-READ-read_dbCAN.R
@@ -1,24 +1,19 @@
-#' @title Read the output of KofamScan/KofamKoala or KAAS.
-#' @description read_ko calculates the abundance of each KO within the 
-#' bins based on the KofamScan or KofamKoala output.
-#' @usage read_ko(data_kofam=NULL, data_kaas=NULL, data_interpro=NULL)
-#' @param data_kofam a path where KofamScan/KofamKoala output data are. They 
+#' @title Read the output of dbCAN
+#' @description read_dbcan calculates the abundance of each Gene within the 
+#' bins based on the dbCAN output.
+#' @usage read_dbcan(dbcan_path)
+#' @param dbcan_path a path where dbCAN output data are. They 
 #' should have the extension .txt and all files in the path are the ones that
-#' need to be read. Output data should have 5 columns with the bin names 
-#' followed by the scaffold name divided by a '-' or '_': bin_scaffoldXX.
-#' @param data_kaas a data frame with 2 columns. Contigs are expected to 
-#' indicate in their names the bin name followed by the scaffold name 
-#' divided by a '-' or '_': bin_scaffoldXX. 
-#' @param data_interpro a data frame output of read_interpro. This
-#' argument is used within mapping_KO.
+#' need to be read. Output data should have 6 columns with the bin names 
+#' followed by the Genes obtained in every algorithm (HMMER,Hotpep,DIAMOND), 
+#' column 'Signalp' indcating if a Peptide signal is found and a column 
+#' '#ofTools" indicating the number of algorithms that found this Gene. 
 #' @details This function is part of a package used for the analysis 
 #' of bins metabolism.
-#' @import dplyr tidyr readr stringr rlang
-#' @importFrom utils read.table
-#' @importFrom purrr map_dfr 
+#' @import dplyr tidyr readr stringr rlang tidyselect
 #' @examples
 #' \dontrun{
-#' read_ko("C:/Users/bins/")
+#' read_dbcan("C:/Users/bins/")
 #' }
 #' @export
 
@@ -33,37 +28,37 @@ read_dbcan<-function(dbcan_path){
   # Reading data ----------------------------------------------------------####
   dbcan_df_format<- suppressWarnings(
     dbcan_df %>%
-      filter( `#ofTools` >1) %>%
-      rename(Bin_name=`Gene ID`) %>%
-      mutate( hmmer2=str_replace_all(HMMER, "[[:punct:]]",  "\t")) %>%
-      separate(hmmer2, c("dbNamesHMM"),  sep="\t") %>%
-      mutate(hotpep2=str_replace_all(Hotpep, "[[:punct:]]", "\t")) %>%
-      separate(hotpep2, c("dbNameshotpep"), sep="\t") %>%
-      mutate(diamond2=str_replace_all(DIAMOND, "[[:punct:]]", "\t")) %>%
-      separate(diamond2, c("dbNamesdiamond"),msep="\t") %>%
-      nite("dbCAN_names", dbNamesHMM, dbNameshotpep,  dbNamesdiamond, 
+      filter( .data$`#ofTools` >1) %>%
+      rename(Bin_name=.data$`Gene ID`) %>%
+      mutate( hmmer2=str_replace_all(.data$HMMER, "[[:punct:]]",  "\t")) %>%
+      separate(.data$hmmer2, c("dbNamesHMM"),  sep="\t") %>%
+      mutate(hotpep2=str_replace_all(.data$Hotpep, "[[:punct:]]", "\t")) %>%
+      separate(.data$hotpep2, c("dbNameshotpep"), sep="\t") %>%
+      mutate(diamond2=str_replace_all(.data$DIAMOND, "[[:punct:]]", "\t")) %>%
+      separate(.data$diamond2, c("dbNamesdiamond"), sep="\t") %>%
+      unite("dbCAN_names", .data$dbNamesHMM, .data$dbNameshotpep,  .data$dbNamesdiamond, 
            sep="_", remove = F) %>%  
-      mutate(dbCAN_names=str_replace_all(dbCAN_names, "^_", "")) %>%
-      separate(dbCAN_names, c("dbCAN_names"), sep="_") %>%
-      utate(dbCAN = case_when(str_detect(dbCAN_names, "CBM") ~ 
+      mutate(dbCAN_names=str_replace_all(.data$dbCAN_names, "^_", "")) %>%
+      separate(.data$dbCAN_names, c("dbCAN_names"), sep="_") %>%
+      mutate(dbCAN = case_when(str_detect(.data$dbCAN_names, "CBM") ~ 
                                 "carbohydrate-binding module [CBM]",
-                              str_detect(dbCAN_names, "CE") ~ 
+                              str_detect(.data$dbCAN_names, "CE") ~ 
                                 "carbohydrate esterases [CEs]",
-                              str_detect(dbCAN_names, "GH") ~ 
+                              str_detect(.data$dbCAN_names, "GH") ~ 
                                 "glycoside hydrolases [GHs]",
-                              str_detect(dbCAN_names, "GT") ~ 
+                              str_detect(.data$dbCAN_names, "GT") ~ 
                                 "glycosyltransferases [GTs]",
-                              str_detect(dbCAN_names, "PL") ~ 
-                                "polysaccharide lyases [PLs]"))  %>%
-      mutate(across(where(is.character), str_trim))) %>%
-    dplyr::select(Bin_name,dbCAN_names) %>%
+                              str_detect(.data$dbCAN_names, "PL") ~ 
+                                "polysaccharide lyases [PLs]")) %>%
+      mutate_if(is.character, str_trim) %>%
+    dplyr::select(.data$Bin_name, .data$dbCAN_names) %>%
     calc_abundance(analysis = "dbCAN") %>% 
-    dplyr::select(-Scaffold_name)
+    dplyr::select(-.data$Scaffold_name))
   # Menssage --------------------------------------------------------------####
   initial<-dim(dbcan_df)
   final<-dim(dbcan_df %>%
-               filter( `#ofTools` >1))
-  signals<- dbcan_df %>% group_by(Signalp) %>% count()
+               filter( .data$`#ofTools` >1))
+  signals<- dbcan_df %>% group_by(.data$Signalp) %>% count()
 
   print(paste0("Input Genes = " , initial[1]))
   print(paste0("Output filtered Genes = " , final[1]))
